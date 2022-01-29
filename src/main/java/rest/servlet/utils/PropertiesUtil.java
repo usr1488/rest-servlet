9f48d25da98810848f3db4ad36907a250293a324
usr1488 <justzone95@gmail.com>
2022-01-29 20:09:33 +0200

    init

---
+++ src/main/java/rest/servlet/utils/PropertiesUtil.java
@@ -0,0 +1,28 @@
+package rest.servlet.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesUtil {
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties");
+
+        if (stream == null) {
+            throw new RuntimeException("Couldn't find application.properties in classpath");
+        }
+
+        try {
+            PROPERTIES.load(stream);
+            stream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return (String) PROPERTIES.get(key);
+    }
+}
